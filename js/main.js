d1115055764a4c49d0ad53b6489a3ad9dfabb496
dvcoffey <dvcoffey@gmail.com>
2020-10-30 08:43:28 +0000

    typo corrected for downPressed function, player movement is now working

--- js/main.js
+++ js/main.js
@@ -8,20 +8,21 @@ let downPressed = false;
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(200, 200, 50, 50);
+    ninja.update();
+    ninja.draw();
     requestAnimationFrame(animate);
 }
 animate();
 
 window.addEventListener('keydown', function(e){
-    if(e.code === ArrowUp) upPressed = true;
+    if(e.keyCode === 38) {upPressed = true}
 })
 window.addEventListener('keyup', function(e){
-    if(e.code === ArrowUp) upPressed = false;
+    if(e.keyCode === 38) {upPressed = false};
 })
 window.addEventListener('keydown', function(e){
-    if(e.code === ArrowDown) upPressed = true;
+    if(e.keyCode === 40) {downPressed = true};
 })
 window.addEventListener('keyup', function(e){
-    if(e.code === ArrowDown) upPressed = false;
+    if(e.keyCode === 40) {downPressed = false};
 })
