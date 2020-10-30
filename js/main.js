3908a2f8146ba5a81fd2e89be01dd1c6c54e198b
dvcoffey <dvcoffey@gmail.com>
2020-10-30 07:04:09 +0000

    event listeners added for keys pressed

--- js/main.js
+++ js/main.js
@@ -2,3 +2,26 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 400;
+
+let upPressed = false;
+let downPressed = false;
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(200, 200, 50, 50);
+    requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener('keydown', function(e){
+    if(e.code === ArrowUp) upPressed = true;
+})
+window.addEventListener('keyup', function(e){
+    if(e.code === ArrowUp) upPressed = false;
+})
+window.addEventListener('keydown', function(e){
+    if(e.code === ArrowDown) upPressed = true;
+})
+window.addEventListener('keyup', function(e){
+    if(e.code === ArrowDown) upPressed = false;
+})
