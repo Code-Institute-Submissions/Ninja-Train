d35f73787fee22c08a6308b69f0cf6dd35906819
dvcoffey <dvcoffey@gmail.com>
2020-10-30 11:26:56 +0000

    Boolean randomizer added to determine which obstacle comes next

--- js/object.js
+++ js/object.js
@@ -20,9 +20,34 @@ class Pylon{
     }
 }
 
+class Bird{
+    constructor(){
+        this.top = 250;
+        this.bottom = 20;
+        this.x = canvas.width;
+        this.width = 20;
+        this.color = 'yellow'
+    }
+
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, canvas.height - this.top, this.width, this.bottom)
+    }
+
+    update(){
+        this.x -= gameSpeed
+        this.draw()
+    }
+}
+
+
+
 function handleObstacles(){
     if (frame%100 === 0){
-        obstaclesArray.unshift(new Pylon);
+        var randomBoolean = Math.random() >= 0.5;
+        console.log(randomBoolean);
+        if(randomBoolean === true){obstaclesArray.unshift(new Pylon);}
+        else{obstaclesArray.unshift(new Bird);}
     }
     for(let i = 0; i < obstaclesArray.length; i++){
     obstaclesArray[i].update();
