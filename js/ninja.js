47a4839ecb0b17ed43c91f5d31578f53e192e690
dvcoffey <dvcoffey@gmail.com>
2020-11-04 15:01:39 +0000

    ninja sprite implemented in game, needs further positioning

--- js/ninja.js
+++ js/ninja.js
@@ -1,9 +1,14 @@
+const ninjaSprite = new Image();
+ninjaSprite.src = '../images/ninja.png'
+
 class Ninja {
     constructor(){
     this.x = 200;
     this.y = 200;
-    this.width = 50;
-    this.height = 50;
+    this.originalWidth = 84;
+    this.originalHeight = 128;
+    this.width = this.originalWidth/2;
+    this.height = this.originalHeight/2;
     };
  
 update(){
@@ -14,6 +19,7 @@ update(){
 draw(){
     ctx.fillStyle = 'red';
     ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.drawImage(ninjaSprite, 0, 0, ninja.width, ninja.height, ninja.x, ninja.y, ninja.width, ninja.height)
 }
 
 jump(){
