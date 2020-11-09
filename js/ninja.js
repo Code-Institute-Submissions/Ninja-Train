0553092f084cec54aff70f23950d179830a6c1b4
dvcoffey <dvcoffey@gmail.com>
2020-11-09 11:55:32 +0000

    img onload added to ninja sprite

--- js/ninja.js
+++ js/ninja.js
@@ -19,7 +19,9 @@ update(){
 draw(){
     //ctx.fillStyle = 'red';
     //ctx.fillRect(this.x, this.y, this.width, this.height)
-    ctx.drawImage(ninjaSprite, 0, 0, this.originalWidth, this.originalHeight, this.x, this.y - 10, this.width * 1.2, this.height * 1.2)
+    img.onload = function () {
+        ctx.drawImage(ninjaSprite, 0, 0, this.originalWidth, this.originalHeight, this.x, this.y - 10, this.width * 1.2, this.height * 1.2)
+    }
 }
 
 jump(){
